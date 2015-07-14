8af3aacbbb51707f4ee935d502dc2379732f58df
Adam Žurek <thejabko@gmail.com>
2015-07-14 13:04:35 +0200

    StatementIterator

---
+++ src/StatementIterator.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace PDOOCI;
+
+
+class StatementIterator implements \Iterator
+{
+    /**
+     * @var array
+     */
+    private $data;
+
+    /**
+     * @param Statement $statement
+     */
+    public function __construct (Statement $statement)
+    {
+        $this->data = $statement->fetchAll();
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Return the current element
+     *
+     * @link http://php.net/manual/en/iterator.current.php
+     * @return mixed Can return any type.
+     */
+    public function current ()
+    {
+        return current($this->data);
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Move forward to next element
+     *
+     * @link http://php.net/manual/en/iterator.next.php
+     * @return void Any returned value is ignored.
+     */
+    public function next ()
+    {
+        next($this->data);
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Return the key of the current element
+     *
+     * @link http://php.net/manual/en/iterator.key.php
+     * @return mixed scalar on success, or null on failure.
+     */
+    public function key ()
+    {
+        return key($this->data);
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Checks if current position is valid
+     *
+     * @link http://php.net/manual/en/iterator.valid.php
+     * @return boolean The return value will be casted to boolean and then evaluated.
+     * Returns true on success or false on failure.
+     */
+    public function valid ()
+    {
+        $k = key($this->data);
+        return isset($this->data[$k]);
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Rewind the Iterator to the first element
+     *
+     * @link http://php.net/manual/en/iterator.rewind.php
+     * @return void Any returned value is ignored.
+     */
+    public function rewind ()
+    {
+        reset($this->data);
+    }
+}
